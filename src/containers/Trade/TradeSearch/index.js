6ba6fc870eee8d6c4d4c7b4a0bea56e99793f77d
zevans98 <zman6221998@gmail.com>
2019-02-05 20:54:48 -0700

    cs

--- src/containers/Trade/TradeSearch/index.js
+++ src/containers/Trade/TradeSearch/index.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
 import card from './cards'
 import pokeSearch from './search';
-import $ from 'jquery'
+import $ from 'jquery';
+import './style.css'
 export class TradeSearch extends Component {
 
     
 
      
     render() {
+        const styles ={
+
+        }
         
         console.log()
         return <div>
-            <button id="button" onClick ={pokeSearch.bind(this) }></button>
-            <input id ="textBox" type='text'></input>
+          <div id="searchDiv" className="container"><input id ="textBox" className="form-control-lg" type='text'></input>
+            </div>  
+            <div id="searchDiv"><button id="button" className="btn btn-danger" onClick ={pokeSearch.bind(this) }>SEARCH</button></div>
             <div className = 'container'>
             <div id="images" className='row'>
             
--- src/containers/Trade/TradeSearch/search.js
+++ src/containers/Trade/TradeSearch/search.js
@@ -1,7 +1,9 @@
+import { types } from 'util';
+
 const axios =  require('axios');
 
 const $ = require('jquery');
-=======
+
 
 
 function pokeSearch(){
@@ -11,15 +13,15 @@ function pokeSearch(){
        // var image = response.sprites.front_shiny;
                   //  var body = response.name;
                   console.log(response);
-                   console.log(response.data.sprites.front_shiny);
-
+                   console.log(response.data.types[0].type.name);
+                     var types = response.data.types[0].type.name;
                    var image = response.data.sprites.front_shiny;
                    var body = response.data.name;
-                   var pokeImage = $('<div class="col-md-4" <div class="card" style="width: 18rem;"><img src="'+image+' " class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+body+'</h5><p class="card-text">pokemon</p></div> </div></div> ');
+                   var pokeImage = $('<div id="sprites"style="border:solid;margin-top:5%;background-color:white" class="col-md-4" <div class="card" style="width: 18rem;"><img src="'+image+' " class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+body+'</h5><p class="card-text">TYPE:  '+types+'</p><button type="button" class="btn btn-danger">ADD</button></</div> </div></div> ');
 
                    //pokeImage.attr("src", image);
                    console.log(pokeImage.attr)
-=======
+
 
                     //pokeImage.attr("src", image);
                     //console.log(pokeImage.attr)
