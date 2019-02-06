3620b86b38e22bbabd359779466e0160276cdf07
RCLARK4958 <rickdclark48@gmail.com>
2019-02-05 19:18:28 -0700

    remove_steam

---
+++ src/containers/Trade/TradeSearch/search.js
@@ -0,0 +1,36 @@
+const axios =  require('axios');
+// const jquery = require('jquery');
+
+function pokeSearch(){
+   var pokeName = document.getElementById('textBox').value;
+   console.log(pokeName);
+    axios.get('https://pokeapi.co/api/v2/pokemon/'+pokeName).then(function(response){
+       // var image = response.sprites.front_shiny;
+                  //  var body = response.name;
+                   console.log(response.data.sprites.front_shiny);
+                    
+                   function show_image(src, width, height, alt) {
+                    var img = document.createElement("img");
+                    img.src = response.data.sprites.front_shiny
+                    img.width = 500;
+                    img.height = 500;
+                    img.alt = "howdy";
+                
+                    // This next line will just add it to the <body> tag
+                    document.body.appendChild(img);
+                }
+                
+                
+                
+                show_image();
+                    //pokeImage.attr("src", image);
+                    //console.log(pokeImage.attr)
+
+                   document.getElementById("images").appendChild
+                   
+    })
+}
+
+
+
+ export default pokeSearch;
