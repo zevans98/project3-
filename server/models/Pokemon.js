08ed236f73114887eb63cdaec14b833566a19029
zevans98 <zman6221998@gmail.com>
2019-02-09 13:52:11 -0700

    coloors

--- server/models/Pokemon.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PokemonSchema = new mongoose.Schema({
-    name: String,
-    sprite: String,
-    type: String
-});
-
-module.exports = mongoose.model('Pokemon', PokemonSchema);
