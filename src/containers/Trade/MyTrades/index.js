e9aa783640c56e2b827487f4efa1b06d357f9257
zevans98 <zman6221998@gmail.com>
2019-02-12 20:11:50 -0700

    cards

--- src/containers/Trade/MyTrades/index.js
+++ src/containers/Trade/MyTrades/index.js
@@ -3,6 +3,11 @@ import card from './cards'
 import $ from 'jquery';
 import './style.css'
 import axios from "axios";
+import { dim } from 'ansi-colors';
+import { domainToUnicode } from 'url';
+import { types } from 'util';
+import pokeSearch from './../TradeSearch/search';
+import { $for } from 'core-js';
 
 
 export class MyTrades extends Component {
@@ -17,17 +22,41 @@ export class MyTrades extends Component {
       .then(res => {
         const pokemons = res.data;
         this.setState({ pokemons });
-      })
-  }
+        console.log(pokemons)
+      });
 
+  };
+ 
+  
+
+  
+  myFunction =(item,index)=>{
+    var pokeBody = $('<div className="col col-md-4"<div  id="mainBox" class="card" style="width: 18rem;"><img src='+this.state.pokemons[index].sprite+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+this.state.pokemons[index].name+'</h5><p class="card-text">'+this.state.pokemons[index].type+'</p></div></div></div>')
+  console.log(this.state.pokemons[index].name);
+  $('#displayPokemon').append(pokeBody)
+}
+  
   render() {
-    return (
-      <ul>
-        { this.state.pokemons.map(pokemon => <li>{pokemon.name}</li>)}
-        { this.state.pokemons.map(pokemon => <li>{pokemon.sprite}</li>)}
-        { this.state.pokemons.map(pokemon => <li>{pokemon.type}</li>)}
+      
+      return (
+     
+      <div>
+        <div className="container-fluid" >
 
-      </ul>
+        <div  id="displayPokemon" className= 'row'>
+        
+        </div>
+        
+        
+        </div>
+
+          {this.state.pokemons.forEach(this.myFunction)}
+        
+
+          
+
+      </div>
+      
     )
   }
 }
