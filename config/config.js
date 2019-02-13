1d80d2205c4bfea95c6009f3551fe9a67f0eba37
RCLARK4958 <rickdclark48@gmail.com>
2019-02-13 14:44:47 -0700

    connect

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://username:password@url:port/db',
+  db: 'mongodb://therick:poopies1@ds335275.mlab.com:35275/heroku_8dxx5gp2',
   db_dev: 'mongodb://localhost:27017/pokemon',
 };
