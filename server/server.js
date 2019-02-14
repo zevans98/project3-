8960d24d61504be6bdf63b23f96967c912f586c4
RCLARK4958 <rickdclark48@gmail.com>
2019-02-13 22:16:40 -0700

    remove_middleware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const fs = require('fs');
-const historyApiFallback = require('connect-history-api-fallback');
+// const historyApiFallback = require('connect-history-api-fallback');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors')
 const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
+// const webpackDevMiddleware = require('webpack-dev-middleware');
+// const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const config = require('../config/config');
-const webpackConfig = require('../webpack.config');
+// const webpackConfig = require('../webpack.config');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const port  = process.env.PORT || 3001;
@@ -37,43 +37,43 @@ app.use(express.json());
 // API routes
 require('./routes')(app);
 
-if (isDev) {
-  const compiler = webpack(webpackConfig);
+// if (isDev) {
+//   const compiler = webpack(webpackConfig);
 
-  app.use(historyApiFallback({
-    verbose: false
-  }));
+//   app.use(historyApiFallback({
+//     verbose: false
+//   }));
 
-  app.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    contentBase: path.resolve(__dirname, '../client/public'),
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  }));
+//   app.use(webpackDevMiddleware(compiler, {
+//     publicPath: webpackConfig.output.publicPath,
+//     contentBase: path.resolve(__dirname, '../client/public'),
+//     stats: {
+//       colors: true,
+//       hash: false,
+//       timings: true,
+//       chunks: false,
+//       chunkModules: false,
+//       modules: false
+//     }
+//   }));
 
-  app.use(webpackHotMiddleware(compiler));
-  app.use(express.static(path.resolve(__dirname, '../dist')));
-} else {
-  app.use(express.static(path.resolve(__dirname, '../dist')));
-  app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
-    res.end();
-  });
-}
+//   app.use(webpackHotMiddleware(compiler));
+//   app.use(express.static(path.resolve(__dirname, '../dist')));
+// } else {
+//   app.use(express.static(path.resolve(__dirname, '../dist')));
+//   app.get('*', function (req, res) {
+//     res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+//     res.end();
+//   });
+// }
 
-app.listen(port, '0.0.0.0', (err) => {
-  if (err) {
-    console.log(err);
-  }
+// app.listen(port, '0.0.0.0', (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
 
-  console.info('>>> 🌎 Open http://localhost:%s/ in your browser.', port);
-});
+//   console.info('>>> 🌎 Open http://localhost:%s/ in your browser.', port);
+// });
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
