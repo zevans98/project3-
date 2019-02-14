9c293db236bccc75c16feaa86837cc6a104c6a4b
RCLARK4958 <rickdclark48@gmail.com>
2019-02-13 22:20:33 -0700

    no_config

--- config/config.js
+++
@@ -1,7 +0,0 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-  MONGODB_URI: "mongodb://heroku_8dxx5gp2:k1vscn31amnhr5fd8jp1bs51mq@ds335275.mlab.com:35275/heroku_8dxx5gp2",  
-  db: 'mongodb://heroku_8dxx5gp2:k1vscn31amnhr5fd8jp1bs51mq@ds335275.mlab.com:35275/heroku_8dxx5gp2',
-  db_dev: 'mongodb://localhost:27017/pokemon',
-};
