e11bba95262c08aee2fc00deb56d63cb9debde1f
RCLARK4958 <RICKDCLARK48@GMAIL.COM>
2019-02-14 14:54:02 -0700

    refresh

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import {store} from './redux/helpers';
 
@@ -12,4 +12,4 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-// registerServiceWorker();
+registerServiceWorker();
